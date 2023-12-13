32a42a358893b14de3e598978457fc9e70a73117
Albert Ákos <albertakos20@gmail.com>
2023-12-13 02:36:57 +0100

    Repositories have been created

---
+++ SpringApp/src/main/java/com/akos/database/repositories/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.akos.database.repositories;
+
+import com.akos.database.entities.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+}
+
---
+++ SpringApp/src/main/java/com/akos/database/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.akos.database.repositories;
+
+import com.akos.database.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
