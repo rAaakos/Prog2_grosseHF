24fa1376465b2e0865d6d8beb96a02a7d6deb7c3
Albert Ákos <albertakos20@gmail.com>
2023-12-15 10:15:31 +0100

    Everything is up-to -date- before - Security-implementation

--- SpringApp/src/main/java/com/akos/database/repositories/TaskRepository.java
+++ SpringApp/src/main/java/com/akos/database/repositories/TaskRepository.java
@@ -1,10 +1,16 @@
 package com.akos.database.repositories;
 
 import com.akos.database.entities.TaskEntity;
+import com.akos.database.entities.TaskState;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+    @Query("SELECT a from TaskEntity a WHERE a.state != ?1")
+    Page<TaskEntity> findByStateNot(TaskState taskState, Pageable pageable);
 }
 
--- SpringApp/src/main/java/com/akos/database/repositories/UserRepository.java
+++ SpringApp/src/main/java/com/akos/database/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package com.akos.database.repositories;
 
 import com.akos.database.entities.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    @Query("SELECT a FROM UserEntity a WHERE a.workingStatus ='ACTIVE' ")
+    Page<UserEntity> findUsersWhoAreActivelyWorking(Pageable pageable);
 }
